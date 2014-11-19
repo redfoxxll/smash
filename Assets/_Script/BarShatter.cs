41da0c0f66c8b66adb770dab1b804933fe245e7a
yuwang88 <yuwang6686@yahoo.co.jp>
2014-11-19 14:36:57 +0800

    update

--- Assets/_Script/BarShatter.cs
+++ Assets/_Script/BarShatter.cs
@@ -2,12 +2,14 @@
 using System.Collections;
 
 public class BarShatter : MonoBehaviour {
-	void OnCollisionEnter(Collision collision)
+//	void OnCollisionEnter(Collision collision)
+	void OnCollisionExit(Collision collision)
 	{
 //			transform.rigidbody.useGravity = true;
 //		    foreach (Transform child in transform) {
 		transform.rigidbody.useGravity = true;
-		transform.rigidbody.AddForce (new Vector3 (0, 0, 2), ForceMode.VelocityChange);
+		transform.rigidbody.AddForce (new Vector3 (0, 0, 10), ForceMode.VelocityChange);
+		Debug.Log ("collision shatter");
 //			}
 	}
 }
